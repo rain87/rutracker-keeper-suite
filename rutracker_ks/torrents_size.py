3d24825611602befb1ede23d43841d50fd074c96
Vladimir Lushpenko <rain87lvs@gmail.com>
2020-01-11 13:58:53 +0200

    update size script

--- rutracker_ks/torrents_size.py
+++ rutracker_ks/torrents_size.py
@@ -4,6 +4,7 @@
 import logging
 logging.basicConfig(level=logging.INFO)
 import sys
+import os
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
@@ -14,11 +15,20 @@ from bencode import bdecode
 
 
 size = 0
-torrents = scan_folder('/tmp/dl/')
+si_GB = 1000 * 1000 * 1000
+desired_size = 1880 * si_GB
+desired_size = 0
+torrents = scan_folder('/tmp/dl')
+dest_dir = '/tmp/dl/sized/'
 for torrent in torrents:
+    data = None
     with open(torrent) as f:
         data = f.read()
-        if data:
-            size += torrent_size(bdecode(data))
+    if data:
+        size += torrent_size(bdecode(data))
+        if desired_size > 0:
+            os.rename(torrent, dest_dir + torrent.split('/')[-1])
+            if size >= desired_size:
+                break
 
 print '{} torrents: {}'.format(len(torrents), pretty_size(size))
